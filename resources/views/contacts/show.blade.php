35d825ff579c85a6ebad16aedb21ad2726aa064e
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-12-09 10:54:39 -0300

    links para editar e voltar ao index

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <x-forms.contact :disabled="true" :contact="$contact" />
+    <div class="d-flex justify-content-between">
+        <a href="{{ route('contacts.index') }}" class="btn btn-primary">{{ __('Voltar') }}</a>
+        <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-primary">{{ __('Editar') }}</a>
+    </div>
 </x-layout>
