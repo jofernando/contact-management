c1d859255c24c923d6cad9b441c03a955d360fb5
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-12-09 11:03:31 -0300

    componente para o alerta de success

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -1,10 +1,5 @@
 <x-layout>
-    @if (session('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
+    <x-alert-success />
     <form action="{{ route('contacts.update', $contact->id) }}" method="POST" id="contact-update">
         @method('PUT')
         @csrf
